e45f864f70ace74c720138a84d9a80b0a7f2f306
Hamzah Marie <mariehamzah1@gmail.com>
2026-09-14 23:05:30 -0400

    feat: polish responsive navbar

--- js/nav.js
+++ js/nav.js
@@ -4,4 +4,5 @@ const navLinks = document.querySelector('.nav-links');
 // Show navlinks / hide nav links when user clicks the menu button
 menuButton.addEventListener('click', () => {
     navLinks.classList.toggle('is-open');
+    menuButton.classList.toggle('is-open');
 })
