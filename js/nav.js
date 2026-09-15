a7268266fbd9e6dd3940b76f436e4ba08b45d49d
Hamzah Marie <mariehamzah1@gmail.com>
2026-09-14 22:27:08 -0400

    feat: add responsive mobile navigation

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+// Show navlinks / hide nav links when user clicks the menu button
+menuButton.addEventListener('click', () => {
+    navLinks.classList.toggle('is-open');
+})
